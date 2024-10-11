88d5687953535c4274c7b09891cb8f182ae25b55
brendanlyc <brendanlyc@gmail.com>
2024-10-12 01:00:11 +0800

    Updated backend logic for learning_path to return metrics needed for home page topic display

--- backend/app/routers/learning_path.py
+++ backend/app/routers/learning_path.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from ..crud import learning_path as crud_learning_path
+from ..crud import progress as crud_progress
+from ..crud import level as crud_levels
 from ..schemas import learning_path as schemas_learning_path
 from ..database import init_postgres
 
@@ -20,9 +22,34 @@ def read_learning_path(learning_path_id: int, db: Session = Depends(init_postgre
     return db_learning_path
 
 
-@router.get("/",response_model=list[schemas_learning_path.LearningPath])
+@router.get("/{user_id}",response_model=list[schemas_learning_path.LearningPath])
 def read_learning_paths(user_id: int, skip: int = 0, limit: int = 0, db: Session = Depends(init_postgres)):
-    return crud_learning_path.get_learning_paths(db=db,user_id=user_id,skip=skip,limit=limit)
+    learning_paths = crud_learning_path.get_learning_paths(db=db,user_id=user_id,skip=skip,limit=limit)
+
+    if not learning_paths:
+        raise HTTPException(status_code=404, detail = "No learning paths found for this user")
+    
+    user_learning_paths = []
+    for path in learning_paths:
+        progress = crud_progress.get_user_progress(db=db,user_id=user_id,learning_path_id=path.id)
+        levels = crud_levels.get_levels_by_learning_path(db=db,learning_path_id=path.id)
+
+        current_level = progress.current_level if progress else 0
+        total_levels = len(levels)
+
+        if not path.is_completed:
+            current_level_title = levels[current_level-1]
+
+        user_learning_paths.append({
+            "id": path.id,
+            "title": path.title,
+            "created_at": path.created_at,
+            "current_level": current_level,
+            "total_levels": total_levels,
+            "current_level_title": current_level_title
+        })
+
+        return user_learning_paths
 
 
 @router.put("/{learning_path_id}",response_model=schemas_learning_path.LearningPath)
